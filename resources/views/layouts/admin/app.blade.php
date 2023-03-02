72972201d2c1ac2d1fae42a6694a0f9629c1a6e3
amaranthineblu <amaranthineblu@yahoo.com>
2023-03-02 23:24:29 +0100

    image

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -119,7 +119,7 @@
                     <ul class="list">
                         <li>
                             <div class="user-info">
-                                <div class="image"><a href="{{route('view.user')}}"><img src="/User-image/{{$u->image}}"
+                                <div class="image"><a href="/User-image/{{$u->image}}"><img src="/User-image/{{$u->image}}"
                                             alt="User"></a></div>
                                 <div class="detail">
                                     <h4>Dr. {{$u->name}}</h4>
---
+++ resources/views/users/single_research.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.users.app')
+@section('description')
+@section('{{Str::slug($research->title)}}')
+
+@section('content')
+
+@endsection
